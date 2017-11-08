98a2240f1f25976655dc69d59f430f9a0355ed1f
kevin birch <kmb@percolate.com>
2017-11-07 19:20:06 -0500

    refactor golden test to be table driven

--- golden_test.go
+++ golden_test.go
@@ -1,73 +1,103 @@
 package main
 
 import (
-	"fmt"
-	"github.com/sergi/go-diff/diffmatchpatch"
-	"github.com/stretchr/testify/assert"
 	"io/ioutil"
+	"path"
 	"strings"
 	"testing"
+
+	"github.com/sergi/go-diff/diffmatchpatch"
+	"github.com/stretchr/testify/assert"
 )
 
-var golden = []string{
-	"_",
-	"Emptier",
-	"Importer",
-	"Channeler",
-	"Voider",
-	"Namedvaluer",
-	"Multireturner",
-	"Variadic",
-	"Pointer",
-	"Interfacer",
-	"Structer",
-	"Embedder",
-	"Qualifier",
+var (
+	golden = []string{
+		"Importer",
+		"Channeler",
+		"Voider",
+		"Namedvaluer",
+		"Multireturner",
+		"Variadic",
+		"Pointer",
+		"Interfacer",
+		"Structer",
+		"Embedder",
+		"Qualifier",
+	}
+	unsupported = [] string{
+		"_",
+		"Emptier",
+	}
+	dmp = diffmatchpatch.New()
+)
+
+func TestUnsupported(t *testing.T) {
+	for _, interfaceName := range unsupported {
+		t.Run(interfaceName, CheckOneUnsupported)
+	}
+}
+
+func CheckOneUnsupported(t *testing.T) {
+	name := path.Base(t.Name())
+	inputFilename := "testdata/" + strings.ToLower(name) + "_def.go"
+
+	g, err := LoadPackageFiles([]string{inputFilename})
+	if err != nil {
+		t.Fatalf("parsePackage error: %s", err)
+	}
+	got, err := g.Generate([]string{name})
+
+	if err == nil {
+		t.Fatalf("expected error for %s", name)
+	}
+	if got != nil {
+		t.Fatalf("Unexpected output result for %s", name)
+	}
 }
 
 func TestGolden(t *testing.T) {
-	dmp := diffmatchpatch.New()
-
 	for _, interfaceName := range golden {
-		inputFilename := "./testdata/" + strings.ToLower(interfaceName) + "_def.go"
-		outputFilename := "./testdata/" + strings.ToLower(interfaceName) + ".go"
-
-		outputFile, err := ioutil.ReadFile(outputFilename)
-		if err != nil {
-			outputFile = []byte{}
-		}
-
-		g, err := LoadPackageFiles([]string{inputFilename})
-		if err != nil {
-			t.Fatalf("parsePackage error: %s", err)
-		}
-		got, err := g.Generate([]string{interfaceName})
-		if err != nil {
-			t.Fatalf("Generator.Generate error for %s: %s", interfaceName, err)
-		}
-
-		// reset gensyms
-		symGen.Reset()
-		identSymGen.Reset()
-
-		if len(outputFile) == 0 {
-			msg := fmt.Sprintf(`file "%s" does not exist, or interface "%s" should result in an empty file`, outputFilename, interfaceName)
-			assert.Equal(t, outputFile, got, msg)
-			continue
-		} else if len(got) == 0 {
-			t.Fatalf(`"%s" resulted in an emtpy file when the contents of "%s" were expected`, interfaceName, outputFilename)
-		}
-
-		readableOutput := string(outputFile)
-		readableResult := string(got)
-
-		// Only compare everything after the first line to avoid
-		// comparing the generation commands
-		outputStart := strings.Index(readableOutput, "\n")
-		resultStart := strings.Index(readableResult, "\n")
-
-		diffs := dmp.DiffMain(readableOutput, readableResult, false)
-
-		assert.Equal(t, readableOutput[outputStart:], readableResult[resultStart:], dmp.DiffPrettyText(diffs))
+		t.Run(interfaceName, CheckOneGolden)
 	}
+}
+
+func CheckOneGolden(t *testing.T) {
+	name := path.Base(t.Name())
+
+	// reset gensyms
+	symGen.Reset()
+	identSymGen.Reset()
+
+	inputFilename := "./testdata/" + strings.ToLower(name) + "_def.go"
+	outputFilename := "./testdata/" + strings.ToLower(name) + ".go"
+
+	outputFile, err := ioutil.ReadFile(outputFilename)
+	if err != nil {
+		outputFile = []byte{}
+	}
+
+	g, err := LoadPackageFiles([]string{inputFilename})
+	if err != nil {
+		t.Fatalf("parsePackage error: %s", err)
+	}
+	got, err := g.Generate([]string{name})
+	if err != nil {
+		t.Fatalf("Generator.Generate error for %s: %s", name, err)
+	}
+
+	if len(got) == 0 {
+		t.Fatalf("%q resulted in an emtpy file when the contents of %q were expected", name, outputFilename)
+	}
+
+	readableOutput := string(outputFile)
+	readableResult := string(got)
+
+	// Only compare everything after the first line to avoid
+	// comparing the generation commands
+	outputStart := strings.Index(readableOutput, "\n")
+	resultStart := strings.Index(readableResult, "\n")
+
+	diffs := dmp.DiffMain(readableOutput, readableResult, false)
+
+	assert.Equal(t, readableOutput[outputStart:], readableResult[resultStart:], dmp.DiffPrettyText(diffs))
 }
